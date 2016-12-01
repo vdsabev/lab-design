4ec7b27283873534cdf89876117a0e649f6bd7c4
Vladimir Sabev <vdsabev@gmail.com>
2016-12-02 01:01:14 +0200

    Revamped result visualization

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -12,6 +12,9 @@ export class ReportComponent implements OnInit {
   Math = Math;
   report: Report;
 
+  maxLowPercent = 0;
+  maxHighPercent = 0;
+
   constructor(
     private changeDetector: ChangeDetectorRef,
     private router: Router,
@@ -44,30 +47,50 @@ export class ReportComponent implements OnInit {
         const testRef = db.ref(`/tests/${testId}`);
         testRef.off('value');
         testRef.on('value', (testSnapshot: { val: () => Test }) => {
+          // TODO: Fix report disappearing when a test value is updated
           stopLoading();
-          this.report.tests[testId] = _.extend(testSnapshot.val(), test);
+          const reportTest = this.report.tests[testId] = _.extend(testSnapshot.val(), test);
+          this.updateLowAndHighPercents(reportTest);
+
           this.changeDetector.detectChanges();
         });
       });
     });
   }
 
-  getSortedValues(test: Test): number[] {
-    const values = [test.minValue, test.value, test.maxValue];
-    values.sort((a, b) => a - b);
+  updateLowAndHighPercents(test: Test) {
+    const valueRange = test.maxValue - test.minValue;
 
-    return values;
+    if (test.value < test.minValue) {
+      const lowPercent = 100 * (test.minValue - test.value) / valueRange;
+      if (lowPercent > this.maxLowPercent) {
+        this.maxLowPercent = lowPercent;
+      }
+    }
+
+    if (test.value > test.maxValue) {
+      const highPercent = 100 * (test.value - test.maxValue) / valueRange;
+      if (highPercent > this.maxHighPercent) {
+        this.maxHighPercent = highPercent;
+      }
+    }
   }
 
-  getMinValue(test: Test): number {
-    return this.getSortedValues(test)[0];
-  }
 
-  getValue(test: Test): number {
-    return this.getSortedValues(test)[1];
-  }
-
-  getMaxValue(test: Test): number {
-    return this.getSortedValues(test)[2];
+  /**
+   * maxLowPercent = 10
+   * maxHighPercent = 5
+   * ----------|---5-----------------------------------------------------------------------------------------95---|-----
+   *               ^ 100 * (10 + 5) / (100 + 10 + 5)                                                         ^ 100 * (10 + 95) / (100 + 10 + 5)
+   * ----------0--------------------------------------------------------------------------------------------------100---
+   *           ^ 100 * (10 + 0) / (100 + 10 + 5)                                                                  ^ 100 * (10 + 100) / (100 + 10 + 5)
+   * ----5-----|--------------------------------------------------------------------------------------------------|101--
+   *     ^ 100 * (10 - 5) / (100 + 10 + 5)                                                                         ^ 100 * (10 + 101) / (100 + 10 + 5)
+   * 10--------|--------------------------------------------------------------------------------------------------|--105
+   * ^ 100 * (10 - 10) / (100 + 10 + 5)                                                                              ^ 100 * (10 + 105) / (100 + 10 + 5)
+   */
+  getValuePercent(test: Test): number {
+    const valuePercent = 100 * (test.value - test.minValue) / (test.maxValue - test.minValue);
+    return 100 * (valuePercent + this.maxLowPercent) / (100 + this.maxLowPercent + this.maxHighPercent);
   }
 }
