565b66c19330c2545ae358e8dffd549babc46cfd
Vladimir Sabev <vdsabev@gmail.com>
2017-09-25 15:44:02 +0300

    Added timeline indicators selector

--- src/timeline/index.ts
+++ src/timeline/index.ts
@@ -1,10 +1,13 @@
-import { div, pre, svg, line, circle } from 'compote/html';
+import './style.scss';
+
+import { div, label, input, svg, circle, line } from 'compote/html';
 import { FactoryComponent, redraw } from 'mithril';
 
 import * as Services from './services';
 export const TimelineServices = Services;
 
 import { Indicator, IndicatorServices } from '../indicator';
+import * as notify from '../notify';
 import { store } from '../store';
 
 export interface Timeline {
@@ -13,37 +16,89 @@ export interface Timeline {
 }
 
 interface IndicatorWithTimeline extends Indicator {
-  timeline: Timeline[];
+  timeline?: Timeline[];
+  timelineLoaded?: boolean;
 }
 
-// TODO: Allow user to select indicators from dropdown, then draw SVG timeline chart with data
-// https://www.highcharts.com/demo/spline-irregular-time
 interface Attrs extends Partial<HTMLDivElement> {
 }
 
+const timelineOptions = {
+  maxX: 1000,
+  maxY: 500,
+  radius: 2.5,
+  colors: ['red', 'green', 'blue', 'yellow'],
+  defaultColor: 'black'
+};
+
 export const Timeline: FactoryComponent<Attrs> = ({ attrs }) => {
-  const { currentUser } = store.getState();
-  const indicatorId = 'thrombocytes';
-  let timelines: Record<string, Partial<IndicatorWithTimeline>> = {
-    [indicatorId]: {}
+  let indicators: IndicatorWithTimeline[] = [];
+  let selectedIndicators: IndicatorWithTimeline[] = [];
+
+  IndicatorServices.query().then((data) => {
+    indicators = data;
+    redraw();
+  });
+
+  const selectIndicator = (e: Event) => {
+    const el = <HTMLInputElement>e.currentTarget;
+    const indicatorId = el.value;
+    const selectedIndicator = indicators.find((indicator) => indicator.id === indicatorId);
+    if (!selectedIndicator) throw new Error(`Invalid indicator selected: ${indicatorId}`);
+
+    if (el.checked) {
+      if (selectedIndicators.indexOf(selectedIndicator) === -1) {
+        selectedIndicators = [...selectedIndicators, selectedIndicator];
+        getTimeline();
+      }
+    }
+    else {
+      if (selectedIndicators.indexOf(selectedIndicator) !== -1) {
+        selectedIndicators = selectedIndicators.filter((indicator) => indicator !== selectedIndicator);
+      }
+    }
   };
 
-  IndicatorServices.get({ indicatorId }).then((data) => timelines[indicatorId] = { ...timelines[indicatorId], ...data }).then(redraw);
-  TimelineServices.get({ userId: currentUser.auth.uid, indicatorId }).then((data) => timelines[indicatorId].timeline = data).then(redraw);
+  const getTimeline = () => {
+    const { currentUser } = store.getState();
+    selectedIndicators.forEach(async (indicator) => {
+      if (indicator.timelineLoaded) return;
 
-  const radius = 5;
+      indicator.timelineLoaded = true;
+      try {
+        indicator.timeline = await TimelineServices.get({ userId: currentUser.auth.uid, indicatorId: indicator.id })
+        redraw();
+      }
+      catch (error) {
+        indicator.timelineLoaded = false;
+        notify.error(error);
+      }
+    });
+  };
 
   return {
     view: () => (
       div({ class: 'container' }, [
-        pre(JSON.stringify(timelines, null, 2)),
-        svg(<any>{ width: '100%', viewBox: `${-radius} ${radius} 1000 500` }, [
-          circle(<any>{ cx: 0, cy: '100%', fill: 'black', r: radius }),
-          line(<any>{ x1: 0, y1: '100%', x2: '100%', y2: '100%', fill: 'black', style: { strokeWidth: '2px' } }),
-          line(<any>{ x1: 0, y1: '100%', x2: 0, y2: 0, fill: 'black', style: { strokeWidth: '2px' } }),
-          Object.keys(timelines).map((iid) =>
-            timelines[iid].timeline ? timelines[iid].timeline.map((timeline, index) =>
-              circle(<any>{ cx: (100 * index), cy: timeline.value, r: radius, fill: 'red' })
+        div({ class: 'timeline-indicators width-100p height-xxl overflow-auto mb-md bg-neutral-lighter' },
+          indicators.map((indicator) => (
+            label({ class: 'block' }, [
+              input({ type: 'checkbox', value: indicator.id, onchange: selectIndicator }),
+              indicator.name
+            ])
+          ))
+        ),
+
+        svg(<any>{ width: '100%', viewBox: `0 0 ${timelineOptions.maxX} ${timelineOptions.maxY}` }, [
+          line(<any>{ x1: 0, y1: '100%', x2: '100%', y2: '100%', stroke: 'black', style: { strokeWidth: 1 } }),
+          line(<any>{ x1: 0, y1: '100%', x2: 0, y2: 0, stroke: 'black', style: { strokeWidth: 1 } }),
+          selectedIndicators.map((indicator, indicatorIndex) =>
+            indicator.timeline ? indicator.timeline.map((timeline, timelineIndex) =>
+              circle(<any>{
+                cx: (100 * timelineIndex),
+                cy: timelineOptions.maxY - timeline.value,
+                r: timelineOptions.radius,
+                fill: timelineOptions.colors[indicatorIndex] || timelineOptions.defaultColor
+              })
             ) : null
           )
         ])
